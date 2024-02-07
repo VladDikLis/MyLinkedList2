6ed55e01f345f167dfb461f41a8b7cdef510285e
VladLisitsyn <vladlisitsindiklis@yandex.ru>
2024-02-07 20:44:52 +0800

    asd

--- src/main/java/ru/otus/java/basic/homework/MyStack.java
+++ src/main/java/ru/otus/java/basic/homework/MyStack.java
@@ -1,17 +1,4 @@
 package ru.otus.java.basic.homework;
 
 public class MyStack<T> {
-    private MyLinkedList<T> stack;
 
-    public MyStack() {
-        stack = new MyLinkedList<>();
-    }
-
-    public void push(T data) {
-        stack.addFirst(data);
-    }
-
-    public T pop() {
-        return stack.remove(0);
-    }
-}
